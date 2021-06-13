800196a65d3e9f9d211173ea3033bdc921fdc366
Mehrdad Saberi <merhdads@gmail.com>
2021-06-13 14:52:58 +0000

    Clean the code, enable unselected matches, add view all

--- shell_script/sh.sh
+++ shell_script/sh.sh
@@ -5,32 +5,33 @@ echo "------------------------------"
 echo "Welcome to Backup and Restore app!"
 echo "To backup all your apps type ALL"
 echo "Otherwise type the name of the apps one by one"
-echo "Type QUIT when you finished with the names"
+echo "Type FINISH when you finished with the names"
+echo "Type VIEW_ALL to view all apps"
 echo "------------------------------"
 echo ""
 
 all_apps=$($adb shell pm list packages -3)
 cur_apps=()
 
-while true
-do
+while true; do
 	echo -n ">> "
 	read inp
 	if [ $inp == "ALL" ]; then
 		cur_apps=$all_apps
 		break
-	elif [ $inp == "QUIT" ]; then
+	elif [ $inp == "FINISH" ]; then
 		break
+	elif [ $inp == "VIEW_ALL" ]; then
+		echo "${all_apps[@]}"
 	else
 		flg=0
-		for APP in $all_apps
-		do
+		for APP in $all_apps; do
 			if echo "$APP" | grep -q "$inp"; then
 				if [ $flg == 0 ]; then
 					echo "matched with $APP"
 					cur_apps+=($(echo ${APP} | sed 's/\r//g'))
 					flg=1
-					break # optional to comment
+					#break # optional to comment
 				else 
 					echo "unselected candidate $APP"
 				fi
@@ -43,19 +44,16 @@ do
 	fi
 done
 
-#echo "${cur_apps[@]}"
 
-
-for APP in "${cur_apps[@]}"
-do
+for APP in "${cur_apps[@]}"; do
 	echo $APP
 	name=$(echo ${APP} | sed "s/^package://")
 	name2=$($adb shell pm path $name | sed "s/^package://")
-	for pth in $name2
-	do
+	for pth in $name2; do
 		$adb pull /$pth "backup/$name.apk"
 	done
 	$adb backup -f backup/$name.ab $name
 done
+
 #adb backup -all -nosystem -f backup/$name.ab
 read first_

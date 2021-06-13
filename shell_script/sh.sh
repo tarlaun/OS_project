076ba06de562e4db0e4efe5e8ae16e7bde76d29b
Mohammad Mahdavi <m.mahdavi79@gmail.com>
2021-06-13 10:59:12 +0000

    fix name2 for multiple apk apps backup only third-party apps add data backup

--- shell_script/sh.sh
+++ shell_script/sh.sh
@@ -1,14 +1,15 @@
-adb='./adb.exe'
-mkdir -p backup
+mkdir backup -p
 
-for APP in $($adb shell pm list packages )
+
+for APP in $(adb shell pm list packages -3)
 do
   name=$(echo ${APP} | sed "s/^package://")
-  name2=$($adb shell pm path $name | sed "s/^package://")
-  
-  echo $name
-  echo $name2
-  $adb pull $name2 "backup/$name.apk"
+  name2=$(adb shell pm path $name | sed "s/^package://")
+  for pth in $name2
+  do
+    adb pull $pth "backup/$name.apk"
+  done
+#  adb backup -f backup/$name.ab $name
 done
-
+adb backup -all -nosystem -f backup/$name.ab
 read first_

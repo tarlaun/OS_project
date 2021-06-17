39e11fc1058208ef3ce41f740cac1d4c3eaba589
mo-ma <m.mahdavi79@gmail.com>
2021-06-17 13:32:29 +0430

    add files back

--- shell_script/backup.sh
+++ shell_script/backup.sh
@@ -1,70 +1,31 @@
-adb="./adb.exe"
+adb="adb"
 mkdir backup -p
 
-
-
-echo "------------------------------"
-echo "Welcome to Backup and Restore app!"
-echo "To backup all your apps type ALL."
-echo "To choose from a list of your files, type SEL."
-echo "Otherwise type the name of the apps one by one."
-echo "Type FINISH when you are finished with the names."
-echo "Type VIEW_ALL to view all apps."
-echo "------------------------------"
-echo ""
 
 all_apps=$($adb shell pm list packages -3)
 cur_apps=()
 all_flag=0
-
-while true; do
-	echo -n ">> "
-	read inp
-	if [ $inp == "ALL" ]; then
-		for APP in $all_apps; do
-			cur_apps+=($(echo ${APP} | sed 's/\r//g'))
-		done
-		all_flag=1
-		break
-	elif [ $inp == "FINISH" ]; then
-		break
-	elif [ $inp == "VIEW_ALL" ]; then
-		echo "${all_apps[@]}"
-	elif [ $inp == "SEL" ]; then
-		for APP in $all_apps; do
-			while true; do
-		 		echo "Do you want to include $APP? (y/n/break)"
-		 		echo -n ">> "
-		 		read inp
-		 		if [ $inp == "y" ]; then
-		 			cur_apps+=($(echo ${APP} | sed 's/\r//g'))
-		 			break
-		 		elif [ $inp == "n" ]; then
-		 			break
-		 		elif [ $inp == "break" ]; then
-		 			break 2
-		 		else
-		 			echo "invalid input. please try again."
-		 		fi
-		 	done
-		done
-	else
-		selected_app="$(python backup_python.py $inp $all_apps)"
-		if [ $selected_app == "no_match" ]; then
-			echo "no match found"
-		else
-			echo "matched with $selected_app"
-			cur_apps+=($(echo ${selected_app} | sed 's/\r//g'))
-		fi	
-
-	fi
+app_list=()
+for APP in $all_apps; do
+    name=($(echo ${APP} | sed "s/^package://" | sed 's/\r//g'))
+    app_list+=($name)
 done
+selected=$(python alt_backup_gui.py ${app_list[@]})
+
+if [ $selected == "ALL" ]; then
+    for APP in ${app_list[@]}; do
+        cur_apps+=($APP)
+    done
+    all_flag=1
+else
+    for APP in $selected; do
+        cur_apps+=($APP)
+    done
+fi
 
 echo "Now the backup process begins: "
 
-for APP in "${cur_apps[@]}"; do
-	echo $APP
-	name=$(echo ${APP} | sed "s/^package://")
+for name in "${cur_apps[@]}"; do
 	name2=$($adb shell pm path $name | sed "s/^package://")
 	echo $name
 	mkdir "backup/$name" -p
--- shell_script/backup_gui.py
+++ shell_script/backup_gui.py
@@ -1,70 +1,103 @@
 import tkinter as tk
-from tkinter import *
+import sys
+import os
 
+
+def LCSubStr(s, t):
+    # Create DP table
+    n = len(s)
+    m = len(t)
+    dp = [[0 for i in range(m + 1)] for j in range(2)]
+    res = 0
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if (s[i - 1] == t[j - 1]):
+                dp[i % 2][j] = dp[(i - 1) % 2][j - 1] + 1
+                if (dp[i % 2][j] > res):
+                    res = dp[i % 2][j]
+            else:
+                dp[i % 2][j] = 0
+    return res
+
+
+def change_list(a, b, c):
+    search_order = []
+    if not deftext:
+        inputstr = str(sv.get())
+    else:
+        inputstr = ""
+    search_order = sorted(app_list, key=lambda x: -LCSubStr(str(x), inputstr))
+    curselect = [ls.get(i) for i in ls.curselection()]
+    ls.delete(0, tk.END)
+    for i in range(len(search_order)):
+        app = search_order[i]
+        ls.insert(tk.END, app)
+        if app in curselect:
+            ls.select_set(i)
+
+
+def handle_focus_in(a):
+    global deftext
+    if deftext:
+        deftext = False
+        txt_box.delete(0, tk.END)
+        txt_box.config(fg='black')
+
+
+def handle_focus_out(a):
+    global deftext
+    if len(sv.get()) == 0:
+        txt_box.delete(0, tk.END)
+        txt_box.config(fg='grey')
+        txt_box.insert(0, search_text)
+        deftext = True
+
+
+def backup():
+    sel = [ls.get(i) for i in ls.curselection()]
+    if len(sel) == len(app_list):
+        print('ALL')
+        exit(0)
+    elif len(sel) != 0:
+        print(' '.join(sel))
+        exit(0)
+
+
+app_list = [str(x) for x in sys.argv[1:]]
+app_list = list(set(app_list))
 root = tk.Tk()
-root.title("برنامه پشتیبانی‌گیری")
-
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 400
-canvas1 = tk.Canvas(root, width=WINDOW_WIDTH, height=WINDOW_HEIGHT, relief='raised')
-canvas1.pack()
-pixel = tk.PhotoImage(width=1, height=1)
-h0 = 25
-diff = 30
-label1 = tk.Label(root, text='به برنامه پشتیبانی برنامه های اندروید خوش آمدید')
-label1.config(font=('B Nazanin', 17))
-canvas1.create_window(WINDOW_WIDTH / 2, h0, window=label1)
-label2 = tk.Label(root, text='برای انتخاب کردن برنامه های خاص نام هر کدام را وارد کرده و دکمه اضافه کردن را فشار دهید')
-label2.config(font=('B Nazanin', 14))
-canvas1.create_window(WINDOW_WIDTH / 2, h0 + diff, window=label2)
-label3 = tk.Label(root, text=' برای انتخاب کردن همه برنامه ها دکمه همه را فشار دهید')
-label3.config(font=('B Nazanin', 14))
-canvas1.create_window(WINDOW_WIDTH / 2, h0 + (diff * 2), window=label3)
-label4 = tk.Label(root,
-                  text=' در صورتی که قبلاً پشتیبان گیری کرده اید برای رفتن به صفحه بازیابی، دکمه بازیابی را فشار دهید')
-label4.config(font=('B Nazanin', 14))
-canvas1.create_window(WINDOW_WIDTH / 2, h0 + (diff * 3), window=label4)
-entry1 = tk.Entry(root)
-canvas1.create_window(WINDOW_WIDTH / 2, h0 + (diff * 4), window=entry1)
-
-
-def backup_process():
-    print("Backup Process")
-    pass
-
-
-def add_one():
-    print("Add one")
-    pass
-
-
-def add_all():
-    print("Add all")
-    pass
-
-
-def restore():
-    print("Restore")
-    pass
-
-
-commands = dict()
-commands['همه'] = add_all
-commands['اضافه کردن'] = add_one
-commands['بازیابی'] = restore
-commands['پشتیبان گیری'] = backup_process
-
-button_h_start = 150
-i = 0
-button_diff = 35
-button_height = 25
-button_width = 100
-for text in (
-        'بازیابی', 'پشتیبان گیری', 'همه', 'اضافه کردن'):
-    button = tk.Button(root, text=text, image=pixel, height=button_height, width=button_width, command=commands[text],
-                       compound="c")
-
-    canvas1.create_window(WINDOW_WIDTH / 2 - 200, button_h_start + (button_diff * i), window=button)
-    i += 1
-
-root.mainloop()
+root.geometry('500x500')
+root.title("پشتیبان‌گیر آندروید")
+label = tk.Label(text="سلام. لطفا برنامه‌های مورد نظر خود را انخاب کنید")
+label.pack()
+lsfrm = tk.Frame(root)
+lsfrm.pack()
+ls = tk.Listbox(lsfrm, width=30, height=20,
+                selectmode='multiple', exportselection=False)
+ls.pack(side='left', fill='y')
+scroll = tk.Scrollbar(lsfrm, orient='vertical')
+scroll.config(command=ls.yview)
+scroll.pack(side='right', fill='y')
+ls.config(yscrollcommand=scroll.set)
+for app in app_list:
+    ls.insert(tk.END, app)
+sv = tk.StringVar()
+sv.trace_add("write", callback=change_list)
+deftext = True
+search_text = "Search..."
+txt_box = tk.Entry(root, textvariable=sv)
+txt_box.insert(tk.END, search_text)
+txt_box.config(fg='grey')
+txt_box.pack()
+txt_box.bind("<FocusIn>", handle_focus_in)
+txt_box.bind("<FocusOut>", handle_focus_out)
+selb = tk.Button(text="انتخاب همه", command=lambda: ls.selection_set(0, tk.END)
+                 )
+selb.pack()
+deselb = tk.Button(text="پاک کردن انتخاب‌ها",
+                   command=lambda: ls.selection_clear(0, tk.END))
+deselb.pack()
+btn = tk.Button(text="پشتیبان‌گیری", command=backup)
+btn.pack()
+tk.mainloop()
